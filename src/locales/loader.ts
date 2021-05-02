a47bed557ff061882bea3577cf48a9a1edb35377
Lolicon <43792621+Lolicon2110@users.noreply.github.com>
2021-05-02 11:27:02 -0300

    Translation: pt-BR (#2)
    Co-authored-by: Rain <emeraldsnorlax@disroot.org>

--- src/locales/loader.ts
+++ src/locales/loader.ts
@@ -4,6 +4,7 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 export function initialize() {
   register('en-GB', () => import('./en-GB.json'));
   register('en-US', () => import('./en-US.json'));
+  register('pt-BR', () => import('./pt-BR.json'));
   register('uwu', () => import('./uwu.json'));
   init({
     fallbackLocale: 'en-GB',
