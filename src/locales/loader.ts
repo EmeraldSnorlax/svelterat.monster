31eb39e70e7ea04807ae1adc02922f69e2d0aa7e
Rain <emeraldsnorlax@disroot.org>
2021-05-02 14:24:44 +0000

    load pt-br

--- src/locales/loader.ts
+++ src/locales/loader.ts
@@ -3,6 +3,7 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 export function initialize() {
   register('en', () => import('./en.json'));
+  register('pt-BR', () => import('./pt-BR.json'));
   register('uwu', () => import('./uwu.json'));
   init({
     fallbackLocale: 'en',
