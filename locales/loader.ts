e8a1c5c4bda60060debb82fce11ddb516eeca711
Peanut <must946@gmail.com>
2021-05-02 19:53:27 +0200

    Translation: de-DE (#3)
    Co-authored-by: Rain <emeraldsnorlax@disroot.org>

--- locales/loader.ts
+++ locales/loader.ts
@@ -4,6 +4,7 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 export function initialize() {
   register('en-GB', () => import('./en-GB'));
   register('en-US', () => import('./en-US'));
+  register('de-DE', () => import('./de-DE'));
   register('pt-BR', () => import('./pt-BR'));
   register('uwu', () => import('./uwu'));
 
