72d22e198c8de111621aac84748947142a87a6f6
Rain <emeraldsnorlax@disroot.org>
2021-05-02 17:11:14 +0000

    remove uneeded translations

--- locales/en-US.ts
+++ locales/en-US.ts
@@ -1,18 +1,6 @@
 export default
 {
-  page: {
-    title: 'Chroma svelterat.monster',
-  },
   warning: {
     text: 'This site contains pulsing colors, which may be problematic for those with photosensitive epilepsy.',
-    button: 'Continue',
-  },
-  controls: {
-    step: 'Step',
-  },
-  footer: {
-    language: 'Language',
-    source: 'Source on Github - GNU AGPL-3.0',
-    translationNote: "Your language isn't here? Submit a translation on the GitHub repo ⟶",
   },
 };
