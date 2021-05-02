a8911da981bb953ee8ced60b6daa9c8a751be585
mwahaha9 <67479109+mwahaha9@users.noreply.github.com>
2021-05-02 09:43:31 -0400

    en-US (#1)
    Co-authored-by: Rain <emeraldsnorlax@disroot.org>

--- src/locales/loader.ts
+++ src/locales/loader.ts
@@ -2,7 +2,8 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 export function initialize() {
-  register('en', () => import('./en-GB.json'));
+  register('en-GB', () => import('./en-GB.json'));
+  register('en-US', () => import('./en-US.json'));
   register('uwu', () => import('./uwu.json'));
   init({
     fallbackLocale: 'en-GB',
