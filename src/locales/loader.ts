6d0ffa41556eae915917647cc37f649856b6cf89
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-05-02 14:38:56 +0100

    rename en

--- src/locales/loader.ts
+++ src/locales/loader.ts
@@ -2,7 +2,7 @@
 import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 export function initialize() {
-  register('en', () => import('./en.json'));
+  register('en', () => import('./en-GB.json'));
   register('uwu', () => import('./uwu.json'));
   init({
     fallbackLocale: 'en',
