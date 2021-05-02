238f2b266cd259675d82998dd60b1f25c179c710
Peanut <must946@gmail.com>
2021-05-02 19:51:10 +0200

    Create de-DE.ts

---
+++ locales/de-DE.ts
@@ -0,0 +1,18 @@
+export default
+{
+  page: {
+    title: 'Chroma svelterat.monster',
+  },
+  warning: {
+    text: 'Diese Webseite enthält pulsierende Farben, welche nicht für Menschen mit photosensitiver Epilepsie geeignet sind.',
+    button: 'Fortfahren',
+  },
+  controls: {
+    step: 'Schritt',
+  },
+  footer: {
+    language: 'Sprache',
+    source: 'Source on Github - GNU AGPL-3.0',
+    translationNote: "Deine Sprache ist nicht hier? Reiche eine Übersetzung auf der GitHub repo ein! ⟶",
+  },
+};
