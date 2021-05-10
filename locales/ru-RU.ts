936286f79bba3eae6cf41d6a3e171c6c101e02a4
Mikhail Koshkin <d1snin@aol.com>
2021-05-10 21:05:40 +0300

    added russian translation

---
+++ locales/ru-RU.ts
@@ -0,0 +1,18 @@
+export default
+{
+  page: {
+    title: 'Цветной svelterat.monster',
+  },
+  warning: {
+    text: 'Этот вебсайт содержит быстро сменяющие друг друга цвета, которые могут навредить людям с эпилепсией.',
+    button: 'Продолжить',
+  },
+  controls: {
+    step: 'Частота',
+  },
+  footer: {
+    language: 'Язык',
+    source: 'Исходный код на GitHub - GNU AGPL-3.0',
+    translationNote: 'Твоего языка здесь нет? Добавь перевод, используя GitHub репозиторий ⟶',
+  },
+};
